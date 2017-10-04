4fbf0da7ba41853a8fb99608c9f51babbfafc283
SilvestriKevin <silvestrikevin94@gmail.com>
2017-10-04 20:03:59 +0200

    Annate migliori

--- index.php
+++ index.php
@@ -16,7 +16,9 @@ if(!empty($_SESSION['id']) && !empty($_GET['esci']) && $_GET['esci']==1){
 
 //creazione della pagina web
 //leggo il file e lo stampo
-if(!empty($_SESSION['id'])) $stampa = "<li><a title='Area Riservata' class='' href='./php/admin.php' tabindex='' acceskey=''>Area Riservata</a></li><li><a title='Esci dall'Area Riservata class='' href='./index.php?esci=1' tabindex='' accesskey='q'>Esci</a></li>";
+if(!empty($_SESSION['id'])) 
+    $stampa = "<li><a title='Area Riservata' class='' href='./php/admin_panel.php' tabindex='' acceskey=''>Area Riservata</a></li>
+               <li><a title='Esci dall'Area Riservata class='' href='./index.php?esci=1' tabindex='' accesskey='q'>Esci</a></li>";
 else $stampa = "<li><a title='Area Riservata' class='' href='./php/login.php' tabindex='' acceskey=''>Area Riservata</a></li>";
 $pagina = file_get_contents("./html/index.html");
 //rimpiazzo il segnaposto con la lista di articoli e stampo in output la pagina  
--- php/best_years.php
+++ php/best_years.php
@@ -8,44 +8,74 @@ include_once("../include/config.php");
 //inclusione file per funzioni ausiliarie
 include_once("../include/lib.php");
 
-if(isset($_SESSION['id'])) header("Location: ../index.php");
-
 $stampa='';
+$vini='';
+$annate='';
+$description_annata='';
 
+/*
 if(!empty($_COOKIE['error'])){
-    $stampa.="<h1 id='gonna_delete_user'>".$_COOKIE['error']."</h1>";
+    $lista.="<h1 id='error_message'>".$_COOKIE['error']."</h1><br></br>";
     setcookie('error',null);
 }
-
-if(isset($_POST['username']) && isset($_POST['password'])){
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    if(!empty($username) && !empty($password)){
-
-        $sql = "SELECT id_user AS id FROM utenti
-	WHERE username='".$username."'  and password=MD5('".$password."')";
-        $result = mysqli_query($conn, $sql);
-
-        if(mysqli_num_rows($result) == 0){
-            setcookie('error','Hai inserito le credenziali errate');
-            header("Location: login.php");
-        }
-        else{
-            $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-            $_SESSION['id'] = $row['id'];
-            header("Location: ../index.php");
-        }
-    }
-    else{
-        setcookie('error','Alcuni campi risultano vuoti');
-        header("Location: login.php");
-    }
+if(!empty($_COOKIE['info'])){
+    $lista.="<h1 id='error_message'>".$_COOKIE['info']."</h1><br></br>";
+    setcookie('info',null);
 }
+*/
+
+$cont = 6;
+
+//STAMPA LE ANNATE
+$sql = "SELECT annate.* FROM annate WHERE migliore=1";
+$result=mysqli_query($conn,$sql);
+if(mysqli_num_rows($result)!=0) 
+    while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+        $annate.="<li><a title='".$row['anno']."' class='' href='./best_years.php?year=".$row['anno']."' tabindex='".$cont."'>".$row['anno']."</a></li>";
+        $cont++;
+    }
+else  $annate.="<li><h2>Non sono presenti annate.</h2></li>";
+
+//STAMPA LE INFORMAZIONI DELL'ANNATA
+$sql = "SELECT annate.* FROM annate WHERE anno=";
+if(!empty($_GET['year'])) $sql.="'".$_GET['year']."' AND migliore=1";
+else $sql.="(SELECT MAX(anno) FROM annate WHERE migliore=1)";
+$result=mysqli_query($conn,$sql);
+if(mysqli_num_rows($result)!=0){
+    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+    $description_annata.="<li><label>Anno: </label>".$row['anno']."</li>";
+    $description_annata.="<li><label>Descrizione: </label>".$row['descrizione']."</li>";
+    $description_annata.="<li><label>Qualit&agrave;: </label>".$row['qualita']."</li>";
+
+    //STAMPA I VINI DELL'ANNATA
+    $sql = "SELECT vini.* FROM vini WHERE annata='".$row['anno']."'";
+    $result=mysqli_query($conn,$sql);
+    if(mysqli_num_rows($result)!=0)
+        while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+            $vini.="<li><div><img alt='' src=''/><ul>";
+            $vini.="<li><label>Nome: </label>".$row['nome']."</li>";
+            $vini.="<li><label>Tipologia: </label>".$row['tipologia']."</li>";
+            $vini.="<li><label>Vitigno: </label>".$row['vitigno']."</li>";
+            $vini.="<li><label>Gradazione: </label>".$row['gradazione']."</li>";
+            $vini.="</ul></div></li>";
+        }
+    else $vini.="<li><h2>Non sono presenti vini per questa annata.</h2></li>";
+}
+//se viene manomesso l'URL e l'anno inserito non è tra le annate migliori
+else if(!empty($_GET['year']))$description_annata.="<li><h2>L'annata selezionata non risulta tra le migliori.</h2></li>";
+
 
 //creazione della pagina web
 //leggo il file e lo inserisco in una stringa
-$pagina = file_get_contents("../html/login.html");
+if(!empty($_SESSION['id'])) 
+    $stampa = "<li><a title='Area Riservata' class='' href='../php/admin_panel.php' tabindex='' acceskey=''>Area Riservata</a></li>
+               <li><a title='Esci dall'Area Riservata class='' href='../index.php?esci=1' tabindex='' accesskey='q'>Esci</a></li>";
+else $stampa = "<li><a title='Area Riservata' class='' href='../php/login.php' tabindex='' acceskey=''>Area Riservata</a></li>";
+$pagina = file_get_contents("../html/best_years.html");
 //rimpiazzo il segnaposto con la lista di articoli e stampo in output la pagina  
-echo str_replace("[ERRORE]", $stampa, $pagina);
+$pagina = str_replace("[ANNATE]", $annate, $pagina);
+$pagina = str_replace("[DESCRIZIONE_ANNATA]", $description_annata, $pagina);
+$pagina = str_replace("[VINI_ANNATA]", $vini, $pagina);
+echo str_replace("[AREA_RISERVATA]", $stampa, $pagina);
 mysqli_close($conn);
 ?>
