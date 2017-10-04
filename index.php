6b1644104f0acc8d747061ea7f1183693a344be5
SilvestriKevin <silvestrikevin94@gmail.com>
2017-10-04 15:28:04 +0200

    minor fix

--- index.php
+++ index.php
@@ -8,9 +8,19 @@ include_once("./include/config.php");
 //inclusione file per funzioni ausiliarie
 include_once("./include/lib.php");
 
+if(!empty($_SESSION['id']) && !empty($_GET['esci']) && $_GET['esci']==1){
+    unset($_SESSION['id']);
+    header('Location: index.php');
+}
+
 
 //creazione della pagina web
 //leggo il file e lo stampo
-echo file_get_contents("./html/index.html");
+if(!empty($_SESSION['id'])) $stampa = "<li><a title='Area Riservata' class='' href='./php/admin.php' tabindex='' acceskey=''>Area Riservata</a></li><li><a title='Esci dall'Area Riservata class='' href='./index.php?esci=1' tabindex='' accesskey='q'>Esci</a></li>";
+else $stampa = "<li><a title='Area Riservata' class='' href='./php/login.php' tabindex='' acceskey=''>Area Riservata</a></li>";
+$pagina = file_get_contents("./html/index.html");
+//rimpiazzo il segnaposto con la lista di articoli e stampo in output la pagina  
+echo str_replace("[AREA_RISERVATA]", $stampa, $pagina);
+mysqli_close($conn);
 
 ?>
