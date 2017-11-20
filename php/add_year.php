4365296d99d50e35c0971f760b5e3998b05bfa70
Unknown <cristian.pirlog@studenti.unipd.it>
2017-11-20 22:48:22 +0100

    PHP & HTML
    -Modifica Vino: La base
    -Aggiungi Annata: Completo

--- php/add_year.php
+++ php/add_year.php
@@ -10,7 +10,7 @@ include_once("../include/lib.php");
 
 if(!isset($_SESSION['id'])) header("Location: ../index.php");
 
-$user='';
+$annata='';
 $info_errore='';
 
 if(!empty($_COOKIE['info'])){
@@ -22,35 +22,79 @@ if(!empty($_COOKIE['error'])){
     setcookie('error',null);
 }
 
- $sql = "SELECT admin FROM utenti WHERE id_user='".$_SESSION['id']."'";
-            $result=mysqli_query($conn,$sql);
+// qualsiasi tipo di utente può aggiungere una nuova annata
 
-            $row = mysqli_fetch_array($result,MYSQL_ASSOC);
-
-            $user.='<form action="add_user.php" method="post">';        
-
-            if($row['admin'] == 1) { 
-
-                $user.= '<fieldset id="register_fieldset">
+$annata.='<form action="add_year.php" method="post">';        
+$annata.= '<fieldset id="register_fieldset">
                     <ul>
                     <li><span>Tutti i campi sono obbligatori</span></li>
-                    <li><label>Nome:</label>
-                    <input type="text" maxlength="50" name="nome" id="nome" title="nome" tabindex="1"/>
-                    <li><label>Username:</label>
-                    <input type="text" maxlength="50" name="username" id="username" title="username" tabindex="1"/>
-                        </li><li><label>Indirizzo email:</label>
-                    <input type="text" maxlength="50" name="email" id="email" title="email" value="example@gmail.com" onfocus="placeHolder(this);" tabindex="5"/>
-                        </li><li><label>Password:</label>
-                    <input type="password" maxlength="100" name="password" id="password" title="password" tabindex="6"/>
-                        </li><li><label>Conferma Password:</label>
-                    <input type="password" maxlength="100" name="conferma_password" id="conferma_password" title="conferma_password" tabindex="7"/>
-                        </li><li><input type="submit" class="search_button" name="register" value="Salva" accesskey="s" tabindex="8"/></li>
+                    <li><label>Anno:</label>
+                    <input type="text" maxlength="4" name="anno" title="anno" tabindex="1"/>
+                    <li><label>Descrizione:</label>
+                    <input type="textarea" maxlength="50" name="descrizione" title="descrizione" tabindex="1"/>
+                        </li>
+                        <li><label>Qualit&agrave;:</label>
+                    <input type="text" maxlength="100" name="qualita" title="qualita" tabindex="6"/>
+                        </li>
+                        <li><label>Migliore: </label><input type="checkbox" name="migliore" title="migliore" value="1" tabindex="7"/></li>
+                        <li><input type="submit" class="search_button" name="salva" value="Aggiungi" accesskey="s" tabindex="8"/></li>
                     </ul>
 
                 </fieldset>';
 
+if(isset($_POST['anno']) && isset($_POST['descrizione']) && isset($_POST['qualita'])) {
+    // controllo che non siano stati lasciati spazi vuoti all'interno di anno e che tutti i campi siano non vuoti.
 
-            } else $user.="<h2>Non hai accesso a questa pagina.</h2>";
+    if(!empty($_POST['anno']) && !empty($_POST['descrizione']) && !empty($_POST['qualita'])) {
+        //dichiaro le variabili
+
+        $anno = $_POST['anno'];
+        $descrizione = $_POST['descrizione'];
+        $qualita = $_POST['qualita'];
+
+        // controllo che l'anno sia del formato giusto
+
+        if(is_numeric($anno) && strlen($anno)==4 && !preg_match("/^(\s)+$/",$anno)) {
+            // controllo che l'anno non sia già presente all'interno del database
+
+            $sql = "SELECT anno FROM annate WHERE anno='".$anno."'";
+            $result = mysqli_query($conn, $sql);
+
+            if(mysqli_num_rows($result) != 0){
+                setcookie('error',"L'anno inserito esiste gi&agrave;"); 
+                header("Location: add_year.php");
+            } else {
+                // inserisco i dati nel database
+                
+                if(!isset($_POST['migliore']))
+                {   
+                    $sql = "INSERT INTO annate (anno, descrizione,qualita) VALUES ('".$anno."','".$descrizione."', '".$qualita."')";
+                }
+                else { 
+                    $sql = "INSERT INTO annate (anno, descrizione,qualita,migliore) VALUES ('".$anno."','".$descrizione."', '".$qualita."', '1')";
+                }
+                //controllo la connessione
+                if (mysqli_query($conn, $sql) == TRUE) {
+                    setcookie('info',"Aggiunta avvenuta con successo.");
+                    header("Location: admin_panel.php");
+                } else {
+                    setcookie('error',"Si &egrave; verificato un errore. La preghiamo di riprovare");
+                    header("Location: add_year.php");
+                }
+            }
+        } else {
+            setcookie('error','Anno non è nel formato giusto.');
+            header("Location: add_year.php"); 
+        }
+
+    } else {
+        setcookie('error','Alcuni campi risultano vuoti.');
+        header("Location: add_year.php"); 
+    }
+}
+$annata.='</form>';
+
+/*
 
             if(isset($_POST['nome']) && isset($_POST['username']) && isset($_POST['email']) && isset($_POST['password']) && isset($_POST['conferma_password'])) {
 
@@ -110,13 +154,14 @@ if(!empty($_COOKIE['error'])){
             }  
 
 
-            $user.="</form>";
+            $vini.="</form>";
+*/
 
 //creazione della pagina web
 //leggo il file e lo inserisco in una stringa
 $pagina = file_get_contents("../html/admin_panel.html");
 //rimpiazzo il segnaposto con la lista di articoli e stampo in output la pagina  
 $pagina = str_replace("[INFO/ERRORE]", $info_errore, $pagina);
-echo str_replace("[VINI]", $user, $pagina);
+echo str_replace("[VINI]", $annata, $pagina);
 mysqli_close($conn);
 ?>
--- php/modify_wine.php
+++ php/modify_wine.php
@@ -10,35 +10,70 @@ include_once("../include/lib.php");
 
 if(!isset($_SESSION['id'])) header("Location: ../index.php");
 
-$vini='';
 
-//STAMPA I VINI (QUANDO SI APRE LA PAGINA LA PRIMA VOLTA)
-$sql = "SELECT vini.* FROM vini";
-$result=mysqli_query($conn,$sql);
-if(mysqli_num_rows($result)!=0)
-    while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
-        $vini.="<li><div id='specific_result' class='specific_wine'><img alt='' src='../img/".$row['foto'].".png'/>";
-        $vini.="<ul>";
-        $vini.="<li><a title='Modifica vino' class='' href='./modify_wine.php' tabindex='' accesskey=''>Modifica</a></li>";
-        $vini.="<li><a title='Elimina vino' class='' href='./delete_wine.php' tabindex='' accesskey=''>Elimina</a></li>";
-        $vini.="<li><label>Nome: </label>".$row['nome']."</li>";
-        $vini.="<li><label>Denominazione: </label>".$row['denominazione']."</li>";
-        $vini.="<li><label>Tipologia: </label>".$row['tipologia']."</li>";
-        $vini.="<li><label>Vitigno: </label>".$row['vitigno']."</li>";
-        $vini.="<li><label>Annata: </label>".$row['annata']."</li>";
-        $vini.="<li><label>Abbinamento: </label>".$row['abbinamento']."</li>";
-        $vini.="<li><label>Degustazione: </label>".$row['degustazione']."</li>";
-        $vini.="<li><label>Formato: </label>".$row['formato']."</li>";
-        $vini.="<li><label>Gradazione: </label>".$row['gradazione']."</li>";
-        $vini.="</ul></div></li>";
-    }
-else $vini.="<li><h2>Non sono presenti vini.</h2></li>";
 
+$vino='';
+
+if(!empty($_COOKIE['info'])){
+    $info_errore.="<h1>".$_COOKIE['info']."</h1>";
+    setcookie('info',null);
+}
+if(!empty($_COOKIE['error'])){
+    $info_errore.="<h1>".$_COOKIE['error']."</h1>";
+    setcookie('error',null);
+}
+
+
+
+$id_wine=$_GET['idwine'];
+
+$sql = "SELECT vini.* FROM vini WHERE id_wine='".$id_wine."'";
+
+$result = mysqli_query($conn,$sql);
+$row = mysqli_fetch_array($result,MYSQL_ASSOC);
+
+$annata='';
+
+$vino.='<form action="modify_wine.php?idwine="'.$id_wine.'" method="post">';
+
+if(mysqli_num_rows($result)!=0) {
+    $vino.='<ul>';
+    $annata = $row['annata'];
+    
+    // aggiungo tutte le annate 
+    
+    $vino.='<li><label>Annata: </label><select>';
+    $sql = "SELECT annata as anno FROM vini GROUP BY anno ORDER BY anno";
+    $result=mysqli_query($conn,$sql);
+    if(mysqli_num_rows($result)!=0)
+        while($subrow = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+            $vino.="<option value='".$subrow['anno']."'";
+            // faccio in modo che venga selezionata l'annata giusta per questo vino
+            if($annata == $subrow['anno']) $vino.=" selected='selected'";
+            $vino.=">".$subrow['anno']."</option>";
+        }
+    $vino.='</select><a title="Aggiungi utente" class="" href="./add_year.php" tabindex="" accesskey="">Aggiungi Annata</a></li>';
+
+    $vino.='<li><label>Nome: </label><input type="text" maxlength="30" name="nome" title="nome" value="'.$row['nome'].'"</li>';
+    $vino.='<li><label>Tipologia: </label><input type="text" maxlength="30" name="tipologia" title="tipologia" value="'.$row['tipologia'].'"</li>';
+    $vino.='<li><label>Vitigno: </label><input type="textarea" maxlength="30" name="vitigno" title="vitigno" value="'.$row['vitigno'].'"</li>';
+    $vino.='<li><label>Denominazione: </label><input type="text" maxlength="30" name="denominazione" title="" value="'.$row['denominazione'].'"</li>';
+    $vino.='<li><label>Gradazione(%): </label><input type="text" maxlength="30" name="gradazione" title="gradazione" value="'.$row['gradazione'].'"</li>';
+    $vino.='<li><label>Formato(l)   : </label><input type="text" maxlength="30" name="formato" title="formato" value="'.$row['formato'].'"</li>';
+    $vino.='<li><label>Descrizione: </label><input type="textarea"  name="descrizione" title="descrizione" value="'.$row['descrizione'].'"</li>';
+    $vino.='<li><label>Abbinamento: </label><input type="textarea"  name="abbinamento" title="abbinamento" value="'.$row['abbinamento'].'"</li>';
+    $vino.='<li><label>Degustazione: </label><input type="textarea"  name="degustazione" title="degustazione" value="'.$row['degustazione'].'"</li>';
+    $vino.='<li><input type="submit" name="save_profile" id="save_profile_modifications" value="Salva" /></li>';
+    $vino.='</ul>';
+} else 
+    $vino.='<h1>Ci sono dei problemi con il database.</h1>';
+
+$vino.='</form>';
 
 //creazione della pagina web
 //leggo il file e lo inserisco in una stringa
 $pagina = file_get_contents("../html/admin_panel.html");
 //rimpiazzo il segnaposto con la lista di articoli e stampo in output la pagina  
-echo str_replace("[VINI]", $vini, $pagina);
+echo str_replace("[VINI]", $vino, $pagina);
 mysqli_close($conn);
 ?>
