5bbb3f5b86af794efc2bc3a909fc26673bfc72b9
Kevin Silvestri <silvestrikevin94@gmail.com>
2018-01-03 11:29:02 +0100

    Contattaci e Mappa del sito

--- js/mappa.js
+++ js/mappa.js
@@ -1,15 +1,15 @@
 function myMap() {
-    document.getElementById("googleMap").style.height='300px';
+    document.getElementById("googleMap").style.height = '300px';
 
-    var mapProp= {
-        center:new google.maps.LatLng(43.8771664,11.0364335),
+    var mapProp = {
+        center: new google.maps.LatLng(37.481059,14.0422195),
         zoom:12 ,
     };
     src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAaUkxp5oyLvwG5UI0jY5EmMg2InF-uMUA&callback=myMap"
 
-    var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+    var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
     var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(43.8771664,11.0364335),
+        position: new google.maps.LatLng(37.481059,14.0422195),
         map: map,
         title: 'WineNot'});
 }
