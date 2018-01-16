625abe2513ed8e7b596206eb3a846c23c7bb4a26
LoPirlo <cristian.pirlog@studenti.unipd.it>
2018-01-16 17:13:36 +0100

    JS: aggiunta gestione errori Login/Recupero Password

--- js/javascript.js
+++ js/javascript.js
@@ -3,54 +3,101 @@
 function checkEmail() {
     var email = document.getElementById("email").value;
     var email_reg = /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i;
-    var formContactsNoError = true;
+    var isFieldCorrect = true;
     if (!email_reg.test(email)) { // la mail non è quindi del formato giusto
-        document.getElementById("email").style.borderColor = "red";
-        document.getElementById("mail_error").innerHTML = "Formato e-mail non corretto";
-        formContactsNoError = false;
+        // document.getElementById("email").style.borderColor = "red";
+        document.getElementById("mail_error").innerHTML = "Formato 'e-mail' non corretto";
+        isFieldCorrect = false;
     } else {
-        document.getElementById("email").style.border = "1px inset";
+        // document.getElementById("email").style.border = "1px inset";
         document.getElementById("mail_error").innerHTML = " ";
     }
-    return formContactsNoError;
+    return isFieldCorrect;
 }
 
 function checkMailObject() {
     var oggetto = document.getElementsByName("object")[0].value;
-    var formContactsNoError = true;
+    var isFieldCorrect = true;
     if (!oggetto) {
-        document.getElementsByName("object")[0].style.borderColor = "red";
-        document.getElementById("mail_object_error").innerHTML = "Il campo Oggetto non può essere vuoto";
-        formContactsNoError = false;
+        // document.getElementsByName("object")[0].style.borderColor = "red";
+        document.getElementById("mail_object_error").innerHTML = "Il campo 'Oggetto' non può essere vuoto";
+        isFieldCorrect = false;
     } else {
-        document.getElementsByName("object")[0].style.border = "1px inset";
+        // document.getElementsByName("object")[0].style.border = "1px inset";
         document.getElementById("mail_object_error").innerHTML = " ";
     }
-    return formContactsNoError;
+    return isFieldCorrect;
 }
 
 function checkMailMessage() {
     var content = document.getElementById("textarea_form_help").value;
-    var formContactsNoError = true;
+    var isFieldCorrect = true;
     if (!content) {
-        document.getElementById("textarea_form_help").style.borderColor = "red";
-        document.getElementById("mail_content_error").innerHTML = "Il campo Messaggio non può essere vuoto";
-        formContactsNoError = false;
+        // document.getElementById("textarea_form_help").style.borderColor = "red";
+        document.getElementById("mail_content_error").innerHTML = "Il campo 'Messaggio' non può essere vuoto";
+        isFieldCorrect = false;
     } else {
-        document.getElementById("textarea_form_help").style.border = "1px inset";
+        // document.getElementById("textarea_form_help").style.border = "1px inset";
         document.getElementById("mail_content_error").innerHTML = " ";
     }
 
-    return formContactsNoError;
+    return isFieldCorrect;
 }
 
 
 function checkContactForm() {
     // invoco le altre tre funzioni per controllare se c'é qualcosa che non va
-    
+
     var check = checkEmail();
     check = checkMailObject();
     check = checkMailMessage();
     return check;
 
+}
+
+/* FORM LOGIN */
+
+function checkUsername() {
+    var username = document.getElementById("username").value;
+    var isFieldCorrect = true;
+
+    if (!username) {
+        //document.getElementById("username").style.borderColor = "red";
+        document.getElementById("username_error").innerHTML = "Il campo 'Username' non può essere vuoto";
+        isFieldCorrect = false;
+    } else {
+       // document.getElementById("username").style.border = "1px inset";
+        document.getElementById("username_error").innerHTML = " ";
+    }
+
+    return isFieldCorrect;
+}
+
+function checkPassword() {
+    var password = document.getElementById("password").value;
+    var isFieldCorrect = true;
+
+    if (!password) {
+        //document.getElementById("password").style.borderColor = "red";
+        document.getElementById("password_error").innerHTML = "Il campo 'Password' non può essere vuoto";
+        isFieldCorrect = false;
+    } else {
+       // document.getElementById("password").style.border = "1px inset";
+        document.getElementById("password_error").innerHTML = " ";
+    }
+
+    return isFieldCorrect;
+}
+
+function checkLoginForm() {
+    var check = checkUsername();
+    check = checkPassword();
+    return check;
+}
+
+/* RECUPERO PASSWORD */
+
+function checkEmailRecoverPassword() {
+    var check = checkEmail();
+    return check;
 }
